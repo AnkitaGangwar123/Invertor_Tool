7589b31fe0af839cd6a2d33107dbcbdf87add3e2
HFCL\ankita.gangwar <ankita.gangwar@exicom.in>
2024-10-15 10:34:36 +0530

    Updated UI combobox.

---
+++ can_data.py
@@ -0,0 +1,275 @@
+"""
+Updated can_data.py combobox
+"""
+import cantools
+from can import Message
+from can.interfaces.ixxat import IXXATBus, exceptions
+import time
+import binascii
+
+class inverter_data:
+    def __init__(self, baudrate, error_code, device_num, command, destination_address, source_address):
+        # set badurate
+        self.baudrate = baudrate
+        # set can bus
+        self.bus = None
+        self.error_code = error_code
+        self.device_num = device_num
+        self.command = command
+        self.destination_address = destination_address
+        self.source_address = source_address
+        # set can command
+        self.can_command = Message(is_extended_id=True,
+                                   arbitration_id=0x77F,
+                                   dlc=0x07,
+                                   data=[0x00, 0x000, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+        # self.cycle_time = time.time()+5
+        self.message = []
+        self.can_bus()
+
+    def hex_to_binary(self, hex_num):
+        # Convert hex to integer
+        decimal_num = int(hex_num, 16)
+        # Convert integer to binary and remove the '0b' prefix
+        binary_num = bin(decimal_num)[2:]
+        # Ensure the binary representation is 4 bits per hex digit
+        binary_num = binary_num.zfill(8)
+        
+        return binary_num
+    
+    def binary_to_hex(self, binary_num):
+        # Convert binary to integer
+        decimal_num = int(binary_num, 2)
+        # Convert integer to hex and format it to include the '0x' prefix
+        hex_num = hex(decimal_num)
+
+        integer_val = int(hex_num, 16)
+        a = hex(integer_val)
+
+        return a
+
+    def convert_identifier(self):
+        hex_err_code = self.hex_to_binary(self.error_code)
+        hex_device_number = self.hex_to_binary(self.device_num)
+        hex_command = self.hex_to_binary(self.command)[2:]
+        hex_destination_address = self.hex_to_binary(self.destination_address)
+        hex_source_address = self.hex_to_binary(self.source_address)
+        binary_string = hex_err_code + hex_device_number + hex_command + hex_destination_address + hex_source_address
+
+        # print(self.binary_to_hex(binary_string))
+        return self.binary_to_hex(binary_string)
+
+    def can_bus(self):
+        # print("I am here")
+        self.bus = IXXATBus(channel=0,
+                            can_filters=[{"can_id": 0x00, "can_mask": 0x00}],
+                            bitrate=self.baudrate)
+        
+        
+    def read_can_message(self):
+        self.message = self.bus.recv()
+        return(self.message)
+
+    def send_message(self, identifier, data):
+        """
+
+        Args:
+            identifier ([type]): Identifier for CAN command to be sent.
+            data (list): Data to be sent via CAN.
+        """
+        # identifier = identifier.astype(hex)
+        # print(type(identifier))
+        if isinstance(identifier, str) and identifier.startswith("0x"):
+            identifier = int(identifier, 16)  # Convert from hex string to integer
+        
+        command = Message(is_extended_id=True,
+                          arbitration_id=identifier,
+                          dlc=0x08,
+                          data=data)
+        self.bus.send(command)
+        return(self.read_can_message())
+
+    def system_dc_voltage(self):
+        """
+
+        Args:
+            identifier ([type]): Identifier for CAN command to be  sent.
+        """
+        identifier = self.convert_identifier()
+        data=[0x10, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        response = self.send_message(identifier, data)
+        return(response.data)
+
+    def system_dc_current(self):
+        """
+
+        Args:
+            identifier ([type]): Identifier for CAN command to be  sent.
+        """
+        identifier = self.convert_identifier()
+        data=[0x10, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        response = self.send_message(identifier, data)
+        return(response.data)
+
+    def Module_dc_voltage(self):
+        # identifier = 0x2A300F0
+        identifier = self.convert_identifier()
+        data=[0x11, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        flag = 0
+        while flag == 0:
+            response = self.send_message(identifier, data)
+            if list(response.data)[1] == 1 :
+                flag = 1
+                return(response.data)
+
+    def module_dc_current(self):
+        # identifier = 0x2A300F0
+        identifier = self.convert_identifier()
+        data=[0x11, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        flag = 0
+        while flag == 0:
+            response = self.send_message(identifier, data)
+            if list(response.data)[1] == 2 :
+                flag = 1
+                return(response.data)
+
+    def ac_ab_line_voltage(self):
+        """AI is creating summary for ac_ab_line_voltage
+        """
+        # identifier = 0x2A300F0
+        identifier = self.convert_identifier()
+        data=[0x11, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        flag = 0
+        while flag == 0:
+            response = self.send_message(identifier, data)
+            if list(response.data)[1] == 3:
+                flag = 1
+                return(response.data)
+            
+    def ac_a_phase_current(self):
+        """AI is creating summary for ac_ab_line_voltage
+        """
+        # identifier = 0x2A300F0
+        identifier = self.convert_identifier()
+        data=[0x21, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        flag = 0
+        while flag == 0:
+            response = self.send_message(identifier, data)
+            if list(response.data)[1] == 4 :
+                flag = 1
+                return(response.data)
+
+    def ac_bc_line_voltage(self):
+        # identifier = 0x2A300F0
+        identifier = self.convert_identifier()
+        data=[0x11, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        flag = 0
+        while flag == 0:
+            response = self.send_message(identifier, data)
+            if list(response.data)[1] == 4 :
+                flag = 1
+                return(response.data)
+            
+    def ac_b_phase_current(self):
+        """AI is creating summary for ac_ab_line_voltage
+        """
+        # identifier = 0x2A300F0
+        identifier = self.convert_identifier()
+        data=[0x21, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        flag = 0
+        while flag == 0:
+            response = self.send_message(identifier, data)
+            if list(response.data)[1] == 5 :
+                flag = 1
+                return(response.data)
+
+    def ac_ca_line_voltage(self):
+        # identifier = 0x2A300F0
+        identifier = self.convert_identifier()
+        data=[0x11, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        flag = 0
+        while flag == 0:
+            response = self.send_message(identifier, data)
+            if list(response.data)[1] == 5 :
+                flag = 1
+                return(response.data)
+            
+    def ac_c_phase_current(self):
+        """AI is creating summary for ac_ab_line_voltage
+        """
+        # identifier = 0x2A300F0
+        identifier = self.convert_identifier()
+        data=[0x21, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        flag = 0
+        while flag == 0:
+            response = self.send_message(identifier, data)
+            if list(response.data)[1] == 6 :
+                flag = 1
+                return(response.data)
+
+    def module_ambient_temperature(self):
+        # identifier = 0x2A300F0
+        identifier = self.convert_identifier()
+        data=[0x11, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        flag = 0
+        while flag == 0:
+            response = self.send_message(identifier, data)
+            if list(response.data)[1] == 6 :
+                flag = 1
+                return(response.data)
+            
+    def total_active_power(self):
+        """AI is creating summary for ac_ab_line_voltage
+        """
+        # identifier = 0x2A300F0
+        identifier = self.convert_identifier()
+        data=[0x21, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        flag = 0
+        while flag == 0:
+            response = self.send_message(identifier, data)
+            if list(response.data)[1] == 8 :
+                flag = 1
+                return(response.data)
+            
+    def grid_off_inverter_mode(self):
+        """
+        02 A4 3F F0 21 10 00 00 00 00 00 A2
+        """
+        identifier = 0x02A43FF0
+        data=[0x21, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0xA2]
+        self.send_message(identifier, data)
+
+    def discharge_current_limit_mode(self):
+        identifier = 0x02A43FF0
+        """
+        02 A4 3F F0 11 02 00 00 00 011 E 54
+        """
+        data=[0x11, 0x02, 0x00, 0x00, 0x00, 0x01, 0x1E, 0x54]
+        self.send_message(identifier, data)
+
+    def discharge_current_limit_mode_grid_on(self):
+        identifier = 0x02A43FF0
+        data = [0x10, 0x02, 0x00, 0x00, 0x00, 0x00, 0xC3, 0x50]
+        self.send_message(identifier, data)
+
+    def discharge_cut_off_voltage(self):
+        identifier = 0x02A43FF0
+        data=[0x11, 0x32, 0x00, 0x00, 0x00, 0x04, 0x93, 0xE0]
+        self.send_message(identifier, data)
+
+    def Power_on_all_modules(self):
+        identifier = 0x02A43FF0
+        data=[0x11, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0xA0]
+        self.send_message(identifier, data)
+
+    def Power_on_all_modules_grid_on(self):
+        print("Grid ON")
+        identifier = 0x02A43FF0
+        data=[0x21, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0xA1]
+        self.send_message(identifier, data)
+
+    def set_automatic_switching_mode(self):
+        print("Automatic mode")
+        identifier = 0x02A43FF0
+        data=[0x11, 0x26, 0x00, 0x00, 0x00, 0x00, 0x00, 0xA2]
+        self.send_message(identifier, data)
---
+++ main.py
@@ -0,0 +1,184 @@
+"""
+Updated main.py combo box
+"""
+import sys
+from sys import argv
+from os import getcwd
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton,QGroupBox, \
+    QTableWidget, QTableWidgetItem,QLabel, QLineEdit, QScrollArea, QGridLayout, QComboBox, QFileDialog
+from PyQt5.QtCore import QObject, QThread, pyqtSignal, Qt, QTimer
+from PyQt5.QtGui import QPainter, QPen, QColor, QFont
+
+from can_data import inverter_data
+import time
+import datetime
+
+class read_data_worker(QThread):
+    can_data = pyqtSignal(list)
+
+    def __init__(self, inverter):
+        super().__init__()
+        self.inverter = inverter
+        self.data_list = []
+
+    def run(self):
+        print("Sending commands to set the voltage and current limit of each convertor as required.")
+        self.inverter.Power_on_all_modules_grid_on()
+        self.inverter.set_automatic_switching_mode()
+        self.inverter.discharge_current_limit_mode_grid_on()
+        self.inverter.discharge_cut_off_voltage()
+        self.inverter.Power_on_all_modules()
+        time.sleep(5)
+
+        while True:
+            self.inverter.Power_on_all_modules()
+            self.data_list.append(self.convert_data(self.inverter.Module_dc_voltage()))
+            self.data_list.append(self.convert_data(self.inverter.module_dc_current()))
+            self.data_list.append(self.convert_data(self.inverter.ac_ab_line_voltage()))
+            self.data_list.append(self.convert_data(self.inverter.ac_bc_line_voltage()))
+            self.data_list.append(self.convert_data(self.inverter.ac_ca_line_voltage()))
+            self.data_list.append(self.convert_data(self.inverter.ac_a_phase_current()))
+            self.data_list.append(self.convert_data(self.inverter.ac_b_phase_current()))
+            self.data_list.append(self.convert_data(self.inverter.ac_c_phase_current()))
+            self.data_list.append(self.convert_data(self.inverter.total_active_power()))
+            self.data_list.append(self.convert_data(self.inverter.module_ambient_temperature()))
+            self.can_data.emit(self.data_list)
+
+            self.data_list = []
+            time.sleep(0.5)
+
+    def convert_data(self, response):
+        data = list(response)[4:8]
+        hex_ = ''
+        for a in data:
+            hex_ = hex_+hex(a).lstrip('0x')
+        if hex_ == '':
+            return 0
+        return (int(hex_, 16)/1000)
+
+class biderectional_ui(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Bidirectional Converter.")
+        self.init_ui()
+
+    def init_ui(self):
+        self.centralWidget = QWidget()
+        self.setCentralWidget(self.centralWidget)
+
+        self.main_layout = QHBoxLayout()
+        self.centralWidget.setLayout(self.main_layout)
+
+        # self.inverter = inverter_data(125000,'00', '0A', '23', '00', 'F0')
+        self.inverter = None
+        self.result_file = None
+        self.data_worker = None
+
+        self.start_comm_btn = QPushButton()
+        self.start_comm_btn.setText("Start Communication")
+        self.start_comm_btn.setFixedWidth(200)
+
+        # self.start_comm_btn.clicked.connect(self.read_data)
+
+        # Dropdown to select the respective channel to display the respective CAN-data on the UI
+        self.convertor_selector = QComboBox()
+        self.convertor_selector.addItems(["Convertor", "Convertor_1", "Convertor_2"])
+        # Connect the currentIndexChanged signal of the convertor_selector combo box to the switch_depending_on_convertor_selected function
+        self.convertor_selector.currentIndexChanged[int].connect(self.switch_depending_on_convertor_selected)
+
+        self.main_layout.addWidget(self.convertor_selector)
+        self.main_layout.addWidget(self.start_comm_btn)
+
+        self.parameters = ['DC Voltage',
+                           'DC Current', 
+                           'AC AB Line Voltage', 
+                           'AC BC Line Voltage',
+                           'AC CA Line Voltage',
+                           'A Phase current',
+                           'B Phase current',
+                           'C Phase current',
+                           'Total Active Power',
+                           'Temperature']
+
+        self.parameter_table_widget = QTableWidget()
+        self.main_layout.addWidget(self.parameter_table_widget)
+        self.set_table()
+
+    def set_table(self):
+        self.parameter_table_widget.setRowCount(5)
+        self.parameter_table_widget.setColumnCount(5)
+        self.parameter_table_widget.verticalHeader().setVisible(False)
+        self.parameter_table_widget.horizontalHeader().setVisible(False)
+
+        self.parameter_table_widget.setColumnWidth(1, 75)
+        self.parameter_table_widget.setColumnWidth(0, 200)
+        self.parameter_table_widget.setColumnWidth(2, 20)
+        self.parameter_table_widget.setColumnWidth(3, 200)
+        self.parameter_table_widget.setColumnWidth(4, 75)
+        
+
+        mid = int(len(self.parameters)/2)
+
+        for i in range(0, mid):
+            self.parameter_table_widget.setItem(i, 0, QTableWidgetItem(self.parameters[i]))
+            self.parameter_table_widget.setItem(i, 3, QTableWidgetItem(self.parameters[i+mid]))
+
+    def switch_depending_on_convertor_selected(self, index):
+        text = self.convertor_selector.itemText(index)
+
+        if self.data_worker:  # Check if a DataWorker is already running
+            self.data_worker.quit()  # Quit the current DataWorker
+            self.data_worker = None
+
+        if (text == "Convertor_1"):
+            print("First convertor selected")
+            self.inverter = inverter_data(125000,'00', '0A', '23', '00', 'F0')
+            self.result_file = "result_1.txt"
+            self.read_data()
+        
+        elif (text == "Convertor_2"):
+            print("Second convertor selected")
+            self.inverter = inverter_data(125000,'00', '0A', '23', '01', 'F0')
+            self.result_file = "result_2.txt"
+            self.read_data()
+            
+        else:
+            print("Invalid convertor selected")
+            self.inverter = None
+            self.result_file = None
+
+    def read_data(self):
+        if self.inverter:
+            print("start communication on clicking the push button")
+            self.data_worker = read_data_worker(self.inverter)
+            self.data_worker.can_data.connect(self.update_table)
+            self.data_worker.start()
+        else:
+            print("No valid convertor selected. Please choose a valid convertor.")
+
+    def update_table(self, data):
+        mid = int(len(data)/2)
+        for i in range(0, mid):
+            self.parameter_table_widget.setItem(i, 1, QTableWidgetItem(str(data[i])))
+            self.parameter_table_widget.setItem(i, 4, QTableWidgetItem(str(data[i+mid])))
+        
+        if self.result_file:
+            output_file = open(self.result_file, 'a')
+            output_file.write(str(datetime.datetime.now()))
+            output_file.write(" : ")
+            output_file.write(str(data))
+            output_file.write("\n")
+            # output_file.write(str(temp))
+            output_file.close()
+
+def main():
+    myApp = QApplication(sys.argv)
+    bd_ui = biderectional_ui()
+    bd_ui.setWindowFlags(Qt.WindowCloseButtonHint | Qt.WindowMinimizeButtonHint | Qt.WindowMaximizeButtonHint)
+    bd_ui.show()
+    sys.exit(myApp.exec_())
+
+if __name__ == "__main__":
+    main()
